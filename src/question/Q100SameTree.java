54e6ed4b9743671ed53e614ee75e8a753d0f1b95
kyle64 <244233177@qq.com>
2020-08-07 02:47:05 +0800

    100. 相同的树

---
+++ src/question/Q100SameTree.java
@@ -0,0 +1,82 @@
+package question;
+
+/**
+ * Created by ziheng on 2020/8/7.
+ */
+public class Q100SameTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * @Description: 100. 相同的树
+     *
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     *
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     *
+     * 示例 1:
+     *
+     * 输入:       1         1
+     *           / \       / \
+     *          2   3     2   3
+     *
+     *         [1,2,3],   [1,2,3]
+     *
+     * 输出: true
+     * 示例 2:
+     *
+     * 输入:      1          1
+     *           /           \
+     *          2             2
+     *
+     *         [1,2],     [1,null,2]
+     *
+     * 输出: false
+     * 示例 3:
+     *
+     * 输入:       1         1
+     *           / \       / \
+     *          2   1     1   2
+     *
+     *         [1,2,1],   [1,1,2]
+     *
+     * 输出: false
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/same-tree
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @date 2020/8/7 上午2:46
+     * @param p
+     * @param q
+     * @return boolean
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+
+        // dfs递归
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
